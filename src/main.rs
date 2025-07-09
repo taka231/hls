fed6790036e40812fe3706c1b0229773c0dbc233
taka231 <taka23131@gmail.com>
2025-07-10 03:53:19 +0900

    refactor: use anyhow in main

--- src/main.rs
+++ src/main.rs
@@ -6,47 +6,27 @@ pub mod convert;
 pub mod parser;
 
 use alpha::alpha_convert_program;
+use anyhow::Result;
 use parser::hls;
 
-fn main() {
-    let ast = hls::program(IMPUT);
-    match ast {
-        Ok(program) => {
-            let alpha_converted = alpha_convert_program(&program);
-            match a_normalize::normalize_program(alpha_converted) {
-                Ok(normalized) => {
-                    let mut converter = convert::Converter::init();
-                    match converter.convert(normalized) {
-                        Ok(()) => {
-                            println!("{}", converter.program);
-                        }
-                        Err(e) => {
-                            println!("Conversion error: {}", e);
-                        }
-                    }
-                }
-                Err(e) => {
-                    println!("A-normalization error: {}", e);
-                }
-            }
-        }
-        Err(e) => {
-            println!("Parse error: {}", e);
-        }
-    }
+fn main() -> Result<()> {
+    let program = hls::program(INPUT)?;
+    let alpha_converted = alpha_convert_program(&program);
+    let normalized = a_normalize::normalize_program(alpha_converted)?;
+    let mut converter = convert::Converter::init();
+    converter.convert(normalized)?;
+    println!("{}", converter.program);
+    Ok(())
 }
 
-static IMPUT: &str = r#"
-// external a: i32[16];
-// external b: i32[16];
+static INPUT: &str = r#"
+external a: i32[4];
+// external b: i32[4];
 external out: i32[1];
 
 fn main() = 
-    // let sum_a_b: i32[16] = map(a, b, (x, y) => x + y) in
-    // let squared: i32[16] = map(sum_a_b, (x) => x * x) in
-    // let result: i32 = reduce(squared, (x, y) => x + y) in
-    // out[0] := result
-    let x: i32 = 1 in
-    let y: i32 = 2 in
-    out[0] := x + y * 3
+    // let sum_a_b: i32[4] = map(a, b, (x, y) => x + y) in
+    // let squared: i32[4] = map(sum_a_b, (x) => x * x) in
+    let result: i32 = reduce(a, 0, (x, y) => x + y) in
+    out[0] := result
 "#;
